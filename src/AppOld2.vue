c16bbb103a656d2780eeaae32710de2bec07cf5c
idevforweb <idevforweb@gmail.com>
2022-06-27 13:42:19 -0400

    Finish the Vue Cli

--- src/AppOld2.vue
+++ src/AppOld2.vue
@@ -4,38 +4,22 @@
   <!-- button created to use the toggleModel method -->
   <!-- used .alt event modifier option: alt has to be pressd in order to show modal -->
   <button @click.alt="toggleModal">Show Modal</button>
-  <button @click="toggleModalTwo">Show Modal Two</button>
   <h1> {{ title }} </h1>
   <!-- using v-if to set true or false boolean for modal visibility -->
   <div v-if="showModal">
+    <!-- Add modal as HTML tag -->
+    <!-- Passing header, text, theme props to modal -->
+    <!-- use bind method eg. :header is equal to v-bind:header for props -->
+    <!-- theme is linked to the dynamic :class="{ sales: 'expression' }" set up in Modal.vue -->
+    <!-- @close is the $emit method created in App.vue that selects the toggleModal method to inherit -->
+    <Modal 
+      :header="header"
+      :text="text"
+      :theme="vSales"
+      @close="toggleModal"
+    />
     <!-- Slots are useful for passing custom templates into components -->
-    <!-- slots are writting with defualt closing tag, inline closing tag <modal></modal>-->
-    <Modal :theme="vSales" @close="toggleModal">
-      <!-- name slots dont automitcally render unless called  -->
-      <template v-slot:links>
-        <a href="#">Sign up now</a>
-        <a href="#">More info</a>
-      </template>
-      <!-- defualt slot content  -->
-      <h1>{{ header }}</h1>
-      <p>{{ text }}</p>
-  
-    </Modal>
   </div>
-
-  <teleport to=".modals" v-if="showModalTwo">
-    <Modal @close="toggleModalTwo">
-      <!-- named slots dont automitcally render unless called  -->
-      <template v-slot:links>
-        <a href="#">Sign up now</a>
-        <a href="#">More info</a>
-      </template>
-      <!-- defualt slot content  -->
-      <h1>{{ headerTwo }}</h1>
-      <p>{{ textTwo }}</p>
-  
-    </Modal>
-  </teleport>
 </template>
 
 <script>
@@ -51,27 +35,21 @@ export default {
       title:  'My first Real Vue App :)',
       // Dynamic content for modal prop attributes 'header' and 'text'
       header: 'Dynamic content for header prop',
-      headerTwo: 'Modal Two Header',
       text:   'Dynamic content for modal text prop',
-      textTwo:   'Text for Modal Two',
       vSales: 'sale',
       showModal: false,
-      showModalTwo: false,
     }
   }, 
   methods: {
     toggleModal() {
       this.showModal = !this.showModal;
-    },
-        toggleModalTwo() {
-      this.showModalTwo = !this.showModalTwo;
     }
   }
 }
 </script>
 
 <style>
-#app, .modals {
+#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -5,9 +5,14 @@
         <!-- modal class is static class / binded sale :class is dynamic class set up  -->
         <!-- if binded 'sale class', has a theme that equals sales, it will pick up the styles for that class -->
         <div class="modal" :class="{ sale: theme === 'sale' }">
-            <!-- rendering dynamic content from modal props -->
-            <h1>{{ header }}</h1>
-            <p> {{ text }} </p>
+            <!-- rendering slot content -->
+            <!-- Will render deualt content h1 and p from app.vua -->
+            <slot>This defualt content will only show, if i didnt have the h1 and p elements.</slot>
+            <div class="actions">
+                <!-- by creating slots with the links name, we can show the content of v-slot's -->
+                <slot name="links"></slot>
+            </div>
+            
         </div>
     </div>
 </template>
@@ -15,7 +20,7 @@
 <script>
 export default {
     // exporting dynamic content from App.vue <Modal/> tag
-    props: ['header', 'text', 'theme'],
+    props: ['theme'],
     methods: {
         closeModal() {
             // Creating custom emit event to use .backdrop to close to modal
@@ -28,7 +33,7 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
     .modal {
         width: 400px;
         padding: 20px;
@@ -46,7 +51,19 @@ export default {
     h1 {
         color:aqua;
     }
-
+    .modal .actions {
+        text-align: center;
+        margin: 30px 0 10px 0;
+        color: #333;
+    }
+    .modal .actions a {
+        color:  #333;
+        padding:  8px;
+        border: 1px solid #eee;
+        border-radius: 4px;
+        text-decoration: none;
+        margin: 10px;
+    }
     /* Styling for dynamic modal sale class */
 
     .modal.sale {
